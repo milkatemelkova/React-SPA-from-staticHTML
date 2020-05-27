2e808ad1cfaa7fe736b53758a656383a43434fa0
milkatemelkova <milkatemelkova@abv.bg>
2020-05-27 20:35:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Service from './pages/Service';
+import Single from './pages/Single';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Router>
+    <Header />
+    <Switch>
+   <Route exact path='/' component={Home} />
+   <Route  path='/about' component={About} />
+   <Route  path='/service' component={Service} />
+   <Route  path='/single' component={Single} />
+   <Route  path='/gallery' component={Gallery} />
+   <Route  path='/contact' component={Contact} />
+   </Switch>
+   <Footer />
+   </Router>
+   
   );
 }
 
